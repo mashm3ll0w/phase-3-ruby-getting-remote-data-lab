6f3a59bbcedf3ae6cee1e6e7837f8d2f89aa20f9
Charles Swaleh <charlswall@live.com>
2023-03-29 14:36:13 +0300

    Created the get_response_body instance method

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,4 +7,10 @@ class GetRequester
   def initialize(url)
     @url = url
   end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 end
